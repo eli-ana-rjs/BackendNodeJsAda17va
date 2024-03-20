6775a7b8b7c4ecfec640e163bf61d75ccbb043ed
eli-ana-rjs <elianabellarojas@gmail.com>
2024-03-20 16:33:20 -0300

    Consignas

---
+++ clase18.js
@@ -0,0 +1,19 @@
+// Desarrollar un script que imprima en la consola si un número ingresado es par o impar.
+
+// Desarrollar un script que imprima en la consola el número mayor, de tres números ingresados.
+
+// Dado un número entero n, imprimir todos los números desde n hasta 1 en orden descendente.
+
+// Dado un número entero n, imprimir todos los números desde 1 hasta n en orden ascendente.
+
+// Dado un array de palabras, imprimir la cantidad total de palabras.
+
+// Dado un array de 9 numeros, imprimir el valor booleano true para todos los números pares.
+
+
+// Crear una función que calcule el promedio de edad de un grupo de personas. Recibe una array de edades y devuelve el promedio.
+
+// Crear una función que cuente la cantidad de veces que aparece un elemento en un array. Recibe un array y un elemento como parámetros y devuelve la cantidad de veces que aparece en el array.
+
+// Crea una función que reciba como parámetro una cadena de texto y devuelva la cantidad de vocales que tiene.
+
