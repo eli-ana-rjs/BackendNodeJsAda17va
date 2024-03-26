96062dc4b2869026f5f8b8a03106754a2e00c9f8
eli-ana-rjs <elianabellarojas@gmail.com>
2024-03-25 23:37:00 -0300

    Modificacion funcion para procesar argumentos

---
+++ clase21.js
@@ -0,0 +1,31 @@
+function processesArguments(arguments) {
+
+    const argumentoRecortado = arguments.slice(2);
+
+    const processedArguments = {};
+
+
+    argumentoRecortado.forEach((parametro, index) => {
+        if (parametro.startsWith('--')) {
+            const nombrePropiedad = parametro.slice(2);
+            processedArguments[nombrePropiedad] = argumentoRecortado[index + 1];
+        }
+    });
+
+    for (const propiedad in processedArguments) {
+
+        if (processedArguments[propiedad].includes("_")) {
+
+            processedArguments[propiedad] = processedArguments[propiedad].replaceAll("_", " ");
+        }
+    }
+
+    return processedArguments;
+}
+
+function main() {
+    const processedArguments = processesArguments(process.argv);
+    console.log(processedArguments);
+}
+
+main();
